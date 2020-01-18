5f1fb0b8ddc22276f7c79fd7566f891e9fd35707
Kenneth-joseph <josephochieng812@gmail.com>
2020-01-19 00:56:09 +0300

    build my login and register authentication forms and there views

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 from flask_login import LoginManager
-
+from flask_bootstrap import Bootstrap
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+bootstap = Bootstrap()
+
 
 def create_app(config_name):
 
@@ -13,6 +15,7 @@ def create_app(config_name):
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
     db.init_app(app)
+    bootstap.init_app(app)
     login_manager.init_app(app)
     login_manager.session_protection = 'strong'
     login_manager.login_view = 'auth.login'
