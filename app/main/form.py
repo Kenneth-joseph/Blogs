400ff24416f09a309fd90ff9128f43289b6ebe90
Kenneth-joseph <josephochieng812@gmail.com>
2020-01-20 12:36:28 +0300

    added my blog page and entered the neccessary code for my main section to do the blog and comment section

--- app/main/form.py
+++ app/main/form.py
@@ -6,3 +6,9 @@ from wtforms.validators import Required
 class CommentForm(FlaskForm):
     content=TextAreaField('comment on blog' validator=[Required()])
     submit=SubmitField('comment')
+
+class BlogForm(FlaskForm):
+    title = StringField('Title',validator=[Required])
+    category = StringField('Category',choices=[('Technology','Technology'),('Music','Music'),('Sports','Sports')],validator = [Required])
+    post = TextAreaField('Your blog', validator = [Required])
+    submit = SubmitField('share your blog')
--- app/main/views.py
+++ app/main/views.py
@@ -13,9 +13,37 @@ def index():
     return render_template('index.html')
 
 
-@main.route('/comment')
+@main.route('/blog',methods=['GET','POST'])
 @login_required
-def comment():
+def new_blog():
+    form=BlogForm()
+    if form.validate_on_submit():
+        title=form.title.data
+        category=form.category.data
+        post=form.post.data
+        user_id=current_user
+        new_blog_object = Blog(post=post,title=title,category=category,user_id=current_user._get_current_object().id)
+        new_blog_object.save_blog()
+        return redirect(url_for('main.index'))
+    return render_template('blog.html', form=form)
+
+
+
+@main.route('/comment/<int:blog_id>', methods=['GET','POST'])
+@login_required
+def comment(blog_id):
+    form=CommentForm()
+    blog= Blog.query.get(blog_id)
+    all_comments=Comment.query.filter_by(blog_id=blog_id).all()
+    if form.validate_on_submit():
+        comment=form.content.data
+        blog_id=blog_id
+        user_id = current_user._get_current_object().id
+        new_comment= Comment(comment=comment,user_id=user_id,blog_id=blog_id)
+        new_comment.save_comment()
+        return redirect(url_for('.comment',blog_id=blog_id))
+    return render_template('comment.html',form=form,blog=blog,all_comments=all_comments)
+
 
 
     return render_template('comment.html')
