db0fa99cd11d1d445028e8279e516af2d2c9dc28
Kenneth-joseph <josephochieng812@gmail.com>
2020-01-20 16:59:28 +0300

    added my  requirements and the start.sh file

--- config.py
+++ config.py
@@ -10,7 +10,8 @@ class DevConfig(Config):
 
     DEBUG= True
     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://postgres:kent123kk@localhost/blog'
-    SECRET_KEY = 'kent1234567'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    
 
 
 class ProdConfig(Config):
---
+++ start.sh
@@ -0,0 +1,3 @@
+export SECRET_KEY = 'kent1234567'
+
+python3.6 manage.py server
